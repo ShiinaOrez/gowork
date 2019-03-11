032f944ce23101ba531e3fcece83ae55c2ba463c
ShiinaOrez <shiina_orez@qq.com>
2019-03-11 17:03:06 +0800

    [update]cgroup

--- MyDocker/UTSNamespace.go
+++ MyDocker/UTSNamespace.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-    "os/exec"
-    "syscall"
-    "os"
+    "fmt"
+    "io/ioutil"
     "log"
+    "os"
+    "os/exec"
+    "path"
+    "strconv"
+    "syscall"
 )
 
+const cgroupMemoryHierarchyMount = "/sys/fs/cgroup/memory"
+
 func main() {
-    cmd := exec.Command("sh")
+    if os.Args[0] == "/proc/self/exe" {
+        fmt.Printf("current pid %d", syscall.Getpid())
+        fmt.Println()
+        cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+        cmd.SysProcAttr = & syscall.SysProcAttr {}
+        cmd.Stdin = os.Stdin
+        cmd.Stdout = os.Stdout
+        cmd.Stderr = os.Stderr
+        if err := cmd.Run(); err != nil {
+            fmt.Println(err)
+            os.Exit(1)
+        }
+    }
+
+    cmd := exec.Command("/proc/self/exe")
     cmd.SysProcAttr = &syscall.SysProcAttr {
         Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID |
             syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
@@ -32,8 +52,22 @@ func main() {
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
 
-    if err := cmd.Run(); err != nil {
-        log.Fatal(err)
+    if err := cmd.Start(); err != nil {
+        log.Fatal("Error:", err)
+        os.Exit(-1)
+    } else {
+        fmt.Printf("%v", cmd.Process.Pid)
+
+        os.Mkdir(path.Join(cgroupMemoryHierarchyMount, "testmemorylimit"), 0755)
+        ioutil.WriteFile(
+            path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "tasks"),
+            []byte(strconv.Itoa(cmd.Process.Pid)),
+            0644)
+
+        ioutil.WriteFile(
+            path.Join(cgroupMemoryHierarchyMount, "testmemorylimit", "memory.limit_in_bytes"),
+            []byte("100m"),
+            0644)
     }
-    os.Exit(-1)
+    cmd.Process.Wait()
 }
