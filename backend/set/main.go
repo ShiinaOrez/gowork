18ca8c3ba617b1fc20b3837d5909424883c32a74
ShiinaOrez <shiina_orez@qq.com>
2019-04-05 13:28:46 +0800

    [feat]

--- backend/set/main.go
+++ backend/set/main.go
@@ -1,27 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 type Type int
 
 type Set map[Type]bool
 
 func (set *Set)AddItem(item Type) error {
-    _, ok := *set[item]
+    _, ok := (*set)[item]
     if ok {
         return errors.New("Item already existed!")
     } else {
-        *set[item] = true
+        (*set)[item] = true
     }
     return nil
 }
 
 func (set *Set)Contain(item Type) bool {
-    return _, ok = *set[item]; ok
+    _, ok := (*set)[item]
+    return ok
 }
 
 func (set *Set)Delete(item Type) error {
-    if _, ok = *set[item]; ok {
+    if _, ok := (*set)[item]; ok {
         delete(*set, item)
     } else {
         return errors.New("Item not found!")
@@ -41,19 +45,55 @@ func (set *Set)Items() ([]Type, error) {
     return res, nil
 }
 
-func Add(setA, setB Set) Set {
+func And(setA, setB Set) Set {
     set := make(Set)
     for k, _ := range setA {
         set[k] = true
     }
     for k, _ := range setB {
-        if _, ok := set[k], !ok {
+        if _, ok := set[k]; !ok {
             set[k] = true
         }
     }
     return set
 }
 
+func Or(setA, setB Set) Set {
+    set := make(Set)
+    for k, _ := range setA {
+        if _, ok := setB[k]; ok {
+            set[k] = true
+        }
+    }
+    return set
+}
+
+func (setA *Set)Sub(setB Set) Set {
+    set := *setA
+    for k, _ := range setB {
+        if _, ok := (*setA)[k]; ok {
+            set.Delete(k)
+        }
+    }
+    return set
+}
+
+func test() {
+    seta := make(Set)
+    setb := make(Set)
+    seta.AddItem(1)
+    seta.AddItem(3)
+    seta.AddItem(5)
+    setb.AddItem(2)
+    setb.AddItem(4)
+    setb.AddItem(6)
+    fmt.Println("seta:", seta)
+    fmt.Println("setb:", setb)
+    fmt.Println("seta and setb:", And(seta, setb))
+    fmt.Println("seta or setb:", Or(seta, setb))
+    fmt.Println("seta sub setb:", seta.Sub(setb))
+}
+
 func main() {
-    fmt.Println()
+    test()
 }
