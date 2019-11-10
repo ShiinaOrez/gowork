e622d0769b66b1662706dfa8948510efb58360f1
ShiinaOrez <shiina_orez@qq.com>
2019-11-10 23:24:44 +0800

    [...]

---
+++ go-micro/api-gateway/rpc/proto/api.micro.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+    "fmt"
+    "context"
+    "math"
+    "github.com/golang/protobuf/proto"
+    "github.com/micro/go-micro/client"
+    "github.com/micro/go-micro/server"
+)
+
+const _ = proto.ProtoPackageIsVersion3
+
+type ExampleService interface {
+    Call(ctx context.Context, in *CallRequest, opts ...client.CallOption) (*CallResponse, error)
+}
+
+type exampleService struct {
+    c client.Client
+    name string
+}
+
+// create a new example service
+func NewExampleService(name string, c client.Client) ExampleService {
+    // if c is nil, create a new client
+    if c == nil {
+        c = client.NewClient()
+    }
+    // default service name is "example"
+    if len(name) == 0 {
+        name = "example"
+    }
+    return &exampleService {
+        c: c,
+        name: name,
+    }
+}
+
+func (service *exampleService) Call(ctx context.Context, in *CallRequest, opts ...client.CallOption) (*CallResponse, error) {
+    req := service.c.NewRequest(service.name, "Example.Call", in)
+    out := new(CallResponse)
+    err := service.c.Call(ctx, req, out, opts...)
+    if err != nil {
+        return nil, err
+    }
+    return out, nil
+}
+
