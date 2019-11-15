debc2ad945da635f0f4fffa6e2ca6251ecaa68cc
ShiinaOrez <shiina_orez@qq.com>
2019-11-15 09:10:44 +0800

    [fixed: programma analysis]

---
+++ principleOfCompiling/lexicalAnalysis.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Tuple struct {
+	Syn int
+	Val interface{}
+}
+
+type SymbolMap map[string]int
+
+func (m *SymbolMap) Insert(syms []string, codes []int) {
+	length := len(syms)
+	for i := 0; i < length; i++ {
+		(*m)[syms[i]] = codes[i]
+	}
+	return
+}
+
+var (
+	symbolMap  SymbolMap
+	sourceCode string
+)
+
+func init() {
+	fmt.Println("[log]: Initing...")
+
+	fmt.Printf("[log]: Start to init symbols...")
+	symbolMap = make(map[string]int)
+	symbolMap.Insert([]string{
+		"begin", "if", "then", "while", "do", "end",
+		"+", "-", "*", "/", ":", ":=", "<", "<>", "<=", ">", ">=", "=", ";",
+		"(", ")", "#",
+	}, []int{
+		1, 2, 3, 4, 5, 6,
+		13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26,
+		27, 28, 0,
+	})
+	fmt.Println("ok")
+
+	fmt.Printf("[log]: Start to read the source code from in.source...")
+	bytes, err := ioutil.ReadFile("./in.source")
+	if err != nil {
+		fmt.Println("[Error]: Read Source File Failed!\n\tReason: ", err.Error())
+	}
+	sourceCode = string(bytes)
+	fmt.Println("ok")
+
+	_ = os.Remove("./out.log")
+	f, _ := os.OpenFile("./out.log", os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0755)
+	os.Stdout = f
+}
+
+func lexicalAnalysis() []Tuple {
+	result := []Tuple{}
+	sourceCode = strings.Replace(sourceCode, "\t", " ", -1)
+	sourceCode = strings.Replace(sourceCode, "\n", "", -1)
+
+	words := strings.Split(sourceCode, " ")
+	for _, word := range words {
+		if word == "" {
+			continue
+		} else {
+			for word != "" {
+				syn, res := FindSymbol(word)
+				if res {
+					tuple := Tuple{
+						Syn: symbolMap[syn],
+						Val: syn,
+					}
+					result = append(result, tuple)
+					word = strings.Replace(word, syn, "", 1)
+				} else {
+					isNumber := false
+					if IsNumber(word[0]) { // start with digit
+						isNumber = true
+					}
+					if isNumber { // number
+						index := 0
+						for IsNumber(word[index]) {
+							index++
+							if index == len(word) {
+								break
+							}
+						}
+						number, _ := strconv.Atoi(word[:index])
+						tuple := Tuple{
+							Syn: 11,
+							Val: number,
+						}
+						result = append(result, tuple)
+						word = word[index:]
+					} else if IsNotNumberOrLetter(word[0]) {
+						fmt.Println("Syntex Error At: ", string(word[0]))
+						return []Tuple{}
+					} else { // identifier
+						index := 0
+						for IsNumberOrLetter(word[index]) {
+							index++
+							if index == len(word) {
+								break
+							}
+						}
+						tuple := Tuple{
+							Syn: 10,
+							Val: word[:index],
+						}
+						result = append(result, tuple)
+						word = word[index:]
+					}
+				}
+			}
+		}
+	}
+	return result
+}
+
+func FindSymbol(word string) (string, bool) {
+	if strings.HasPrefix(word, ":=") {
+		return ":=", true
+	}
+	if strings.HasPrefix(word, "<=") {
+		return "<=", true
+	}
+	if strings.HasPrefix(word, "<>") {
+		return "<>", true
+	}
+	if strings.HasPrefix(word, ">=") {
+		return ">=", true
+	}
+
+	for key, _ := range symbolMap {
+		if strings.HasPrefix(word, key) {
+			return key, true
+		}
+	}
+	return "", false
+}
+
+func IsNumber(b byte) bool {
+	return int(b) <= int('9') && int(b) >= int('0')
+}
+
+func IsLetter(b byte) bool {
+	return int(b) <= int('z') && int(b) >= int('a')
+}
+
+func IsNumberOrLetter(b byte) bool {
+	return IsNumber(b) || IsLetter(b)
+}
+
+func IsNotNumberOrLetter(b byte) bool {
+	return !IsNumberOrLetter(b)
+}
--- principleOfCompiling/main.go
+++ principleOfCompiling/main.go
@@ -1,164 +1,188 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
+	"log"
+	_ "fmt"
 )
 
-type Tuple struct {
-	Syn int
-	Val interface{}
-}
-
-type SymbolMap map[string]int
-
-func (m *SymbolMap) Insert(syms []string, codes []int) {
-	length := len(syms)
-	for i := 0; i < length; i++ {
-		(*m)[syms[i]] = codes[i]
-	}
-	return
-}
-
-var (
-	symbolMap  SymbolMap
-	sourceCode string
-	result     []Tuple
-)
-
-func init() {
-	fmt.Println("[log]: Initing...")
-
-	fmt.Printf("[log]: Start to init symbols...")
-	symbolMap = make(map[string]int)
-	symbolMap.Insert([]string{
-		"begin", "if", "then", "while", "do", "end",
-		"+", "-", "*", "/", ":", ":=", "<", "<>", "<=", ">", ">=", "=", ";",
-		"(", ")", "#",
-	}, []int{
-		1, 2, 3, 4, 5, 6,
-		13, 14, 15, 16, 17, 18, 20, 21, 22, 23, 24, 25, 26,
-		27, 28, 0,
-	})
-	fmt.Println("ok")
-
-	fmt.Printf("[log]: Start to read the source code from in.source...")
-	bytes, err := ioutil.ReadFile("./in.source")
-	if err != nil {
-		fmt.Println("[Error]: Read Source File Failed!\n\tReason: ", err.Error())
-	}
-	sourceCode = string(bytes)
-	fmt.Println("ok")
-
-	_ = os.Remove("./out.log")
-	f, _ := os.OpenFile("./out.log", os.O_WRONLY|os.O_CREATE|os.O_SYNC|os.O_APPEND, 0755)
-	os.Stdout = f
-}
-
-func main() {
-	sourceCode = strings.Replace(sourceCode, "\t", " ", -1)
-	sourceCode = strings.Replace(sourceCode, "\n", "", -1)
-
-	words := strings.Split(sourceCode, " ")
-	for _, word := range words {
-		if word == "" {
-			continue
+func is_factor(tuples []Tuple) bool {
+	if len(tuples) == 1 {
+		if tuples[0].Syn == 10 || tuples[0].Syn == 11 {
+			return true;
 		} else {
-			for word != "" {
-				syn, res := FindSymbol(word)
-				if res {
-					tuple := Tuple{
-						Syn: symbolMap[syn],
-						Val: syn,
-					}
-					result = append(result, tuple)
-					word = strings.Replace(word, syn, "", 1)
-				} else {
-					isNumber := false
-					if IsNumber(word[0]) { // start with digit
-						isNumber = true
-					}
-					if isNumber { // number
-						index := 0
-						for IsNumber(word[index]) {
-							index++
-							if index == len(word) {
-								break
-							}
-						}
-						number, _ := strconv.Atoi(word[:index])
-						tuple := Tuple{
-							Syn: 11,
-							Val: number,
-						}
-						result = append(result, tuple)
-						word = word[index:]
-					} else if IsNotNumberOrLetter(word[0]) {
-						fmt.Println("Syntex Error At: ", string(word[0]))
-						return
-					} else { // identifier
-						index := 0
-						for IsNumberOrLetter(word[index]) {
-							index++
-							if index == len(word) {
-								break
-							}
-						}
-						tuple := Tuple{
-							Syn: 10,
-							Val: word[:index],
-						}
-						result = append(result, tuple)
-						word = word[index:]
-					}
-				}
+			log.Println("Error: Item must be indentifier or number or (expression)!")
+			return false
+		}
+	} else if len(tuples) == 0 {
+		log.Println("Error: Item can not be EMPTY string!")
+		return false
+	} else {
+		if tuples[0].Syn == 27 && tuples[len(tuples)-1].Syn == 28 {
+			if res := is_expression(tuples[1:len(tuples)-1]); !res {
+				log.Println("Error: Error with expression!")
+				return false
+			}
+		} else {
+			for _, tuple := range tuples {
+				log.Println(tuple)
+			}
+			log.Println("Error: Expression in item must with `(` and `)` !")
+			return false
+		}
+	}
+	return true
+}
+
+func is_item(tuples []Tuple) bool {
+	// log.Println(tuples)
+	last_end := -1
+	length := len(tuples)
+	if length == 0 {
+		log.Println("Error: Item can not be EMPTY string!")
+		return false
+	}
+	if tuples[0].Syn == 27 {
+		if res := is_factor(tuples); !res {
+			log.Println("Error: Error with factor!")
+			return false
+		}
+		return true
+	}
+	for index, tuple := range tuples {
+		if tuple.Syn == 15 || tuple.Syn == 16 || index == length-1 {
+			if index == length-1 {
+				index += 1
+			}
+			if res := is_factor(tuples[last_end+1:index]); !res {
+				log.Println("Error: Error with factor!")
+				return false
+			} else {
+				last_end = index
 			}
 		}
 	}
-
-	for _, tuple := range result {
-		fmt.Println("(", tuple.Syn, ", ", tuple.Val, ")")
-	}
-	return
+	return true
 }
 
-func FindSymbol(word string) (string, bool) {
-	if strings.HasPrefix(word, ":=") {
-		return ":=", true
+func is_expression(tuples []Tuple) bool {
+	last_end := -1
+	left_bracket := []int{}
+	length := len(tuples)
+	if length == 0 {
+		log.Println("Error: Expression can not be EMPTY string!")
+		return false
 	}
-	if strings.HasPrefix(word, "<=") {
-		return "<=", true
-	}
-	if strings.HasPrefix(word, "<>") {
-		return "<>", true
-	}
-	if strings.HasPrefix(word, ">=") {
-		return ">=", true
-	}
-
-	for key, _ := range symbolMap {
-		if strings.HasPrefix(word, key) {
-			return key, true
+	for index, tuple := range tuples {
+		if tuple.Syn == 27 {
+			left_bracket = append(left_bracket, index)
+			// log.Println(left_bracket)
+		} else if tuple.Syn == 28 {
+			// log.Println(tuples[left_bracket[len(left_bracket)-1]:index+1])
+			if len(left_bracket) == 0 {
+				log.Println("Error: Lack of `(` !")
+				return false
+			}
+			if res := is_item(tuples[left_bracket[len(left_bracket)-1]:index+1]); !res {
+				log.Println("Error: Error with item!")
+				return false
+			}
+			left_bracket = left_bracket[:len(left_bracket)-1]
+			last_end = index
+		} else if tuple.Syn == 13 || tuple.Syn == 14 || index == length-1 {
+			if len(left_bracket) != 0 {
+				continue
+			}
+			if index == length-1 {
+				index += 1
+				if len(left_bracket) != 0 {
+					log.Println("Error: Lack of `)` !")
+					return false
+				}
+			}
+			if last_end == index-1 {
+				last_end = index
+				continue
+			}
+			if res := is_item(tuples[last_end+1:index]); !res {
+				log.Println("Error: Error with item!")
+				return false
+			} else {
+				last_end = index
+			}
 		}
 	}
-	return "", false
+	return true
 }
 
-func IsNumber(b byte) bool {
-	return int(b) <= int('9') && int(b) >= int('0')
+func is_assignment_statement(tuples []Tuple) bool {
+	length := len(tuples)
+	if length == 0 {
+		log.Println("Error: Assignment statement can not be EMPTY string!")
+		return false
+	}
+	if tuples[0].Syn != 10 {
+		log.Println("Error: Assignment statement required indentifier!")
+		return false
+	}
+	if length < 2 || tuples[1].Syn != 18 {
+		log.Println("Error: Assignment statement required `:=` !")
+		return false;
+	}
+	if res := is_expression(tuples[2:]); !res {
+		log.Println("Error: Error with expression!")
+		return false
+	}
+	return true
 }
 
-func IsLetter(b byte) bool {
-	return int(b) <= int('z') && int(b) >= int('a')
+func is_statement(tuples []Tuple) bool {
+	length := len(tuples)
+	if length == 0 {
+		log.Println("Error: Statement can not be EMPTY string!")
+		return false
+	}
+	if res := is_assignment_statement(tuples); !res {
+		log.Println("Error: Error with assignment statement!")
+		return false
+	}
+	return true
 }
 
-func IsNumberOrLetter(b byte) bool {
-	return IsNumber(b) || IsLetter(b)
+func is_statement_string(tuples []Tuple) bool {
+	length := len(tuples)
+	if length == 0 {
+		log.Println("Error: Statement string can not be EMPTY string!")
+		return false
+	}
+	last_end := -1
+	for index, tuple := range tuples {
+		if tuple.Syn == 26 || index == length-1 {
+			if index == length-1 {
+				index += 1
+			}
+			if res := is_statement(tuples[last_end+1:index]); !res {
+				log.Println("Error: Error with statement!")
+				return false
+			} else {
+				last_end = index
+			}
+		}
+	}
+	return true
 }
 
-func IsNotNumberOrLetter(b byte) bool {
-	return !IsNumberOrLetter(b)
+func main() {
+	log.Println("Start: Analysising...")
+	tuples := lexicalAnalysis()
+	length := len(tuples)
+	if tuples[0].Syn != 1 || tuples[length-1].Syn != 0 {
+		log.Println("Error: Please start with `begin` and end with `end`!")
+		return
+	}
+	if res := is_statement_string(tuples[1:length-1]); !res {
+		log.Println("Error: Error with statement string!")
+	} else {
+		log.Println("Analysis Successful!")
+	}
+	return
 }
